12a4306b33df8bcb320f471b4cdeeaa4fc74e33d
Farhad Hedayatifard <hf.farhad@gmail.com>
2025-08-22 10:37:09 +0200

    Fix: Add missing fields to Route model.

--- models/responses/route.go
+++ models/responses/route.go
@@ -8,18 +8,22 @@ import (
 
 // Route represents an AhaSend route
 type Route struct {
-	Object           string    `json:"object"`
-	ID               uuid.UUID `json:"id"`
-	AccountID        uuid.UUID `json:"account_id"`
-	CreatedAt        time.Time `json:"created_at"`
-	UpdatedAt        time.Time `json:"updated_at"`
-	Name             string    `json:"name"`
-	URL              string    `json:"url"`
-	Recipient        string    `json:"recipient,omitempty"`
-	Attachments      bool      `json:"attachments"`
-	Headers          bool      `json:"headers"`
-	GroupByMessageID bool      `json:"group_by_message_id"`
-	StripReplies     bool      `json:"strip_replies"`
-	Secret           string    `json:"secret,omitempty"`
-	Enabled          bool      `json:"enabled"`
+	Object                 string     `json:"object"`
+	ID                     uuid.UUID  `json:"id"`
+	AccountID              uuid.UUID  `json:"account_id"`
+	CreatedAt              time.Time  `json:"created_at"`
+	UpdatedAt              time.Time  `json:"updated_at"`
+	Name                   string     `json:"name"`
+	URL                    string     `json:"url"`
+	Recipient              string     `json:"recipient,omitempty"`
+	Attachments            bool       `json:"attachments"`
+	Headers                bool       `json:"headers"`
+	GroupByMessageID       bool       `json:"group_by_message_id"`
+	StripReplies           bool       `json:"strip_replies"`
+	Secret                 string     `json:"secret,omitempty"`
+	Enabled                bool       `json:"enabled"`
+	ErrorCount             uint64     `json:"error_count"`
+	SuccessCount           uint64     `json:"success_count"`
+	ErrorsSinceLastSuccess int        `json:"errors_since_last_success"`
+	LastRequestAt          *time.Time `json:"last_request_at"`
 }
